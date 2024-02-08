6729499c328925cb191cf1ac9e84f211a2d7d767
Ashish Ranjan Karn <ark.iitkgp@gmail.com>
2024-02-08 19:37:59 +0800

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://cdn-icons-png.flaticon.com/256/1027/1027627.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
